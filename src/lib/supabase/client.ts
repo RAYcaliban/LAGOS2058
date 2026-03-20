a1a3e525c42cf48c5c15312c6081b66a525ec767
Zoso <saadalhamadani6@gmail.com>
2026-03-20 22:29:03 +0300

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -4,15 +4,6 @@ import type { Database } from '@/lib/types/database'
 export function createClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: {
-        flowType: 'pkce',
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        lock: (async (_name: string, _acquireTimeout: number, fn: () => Promise<unknown>) => {
-          return fn()
-        }) as any,
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
